91a2ff955f3b9be10d6b3218b06f3a19855a9509
Andy Aldo Dharmawan <adharmawan@jirnexu.com>
2018-09-05 20:22:40 +0800

    dev: Add logic to robot movement

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Board
+      v-bind:x="x + 1"
+      v-bind:y="maxY - y"
+      v-bind:direction="direction"
+      v-bind:placed="placed"
+    />
+    <Control
+      v-on:place="place"
+      v-on:move="move"
+      v-on:left="left"
+      v-on:right="right"
+      v-on:report="report"
+    />
+    <!-- <h2>{{rep}}</h2> -->
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Board from './components/Board.vue';
+import Control from './components/Control.vue';
+import Direction from './enum';
 
 export default Vue.extend({
   name: 'app',
   components: {
-    HelloWorld,
+    Board,
+    Control,
+  },
+  methods: {
+    place(x: number, y: number, direction: Direction) {
+      this.x = x;
+      this.y = y;
+      this.direction = direction;
+      this.placed = true;
+    },
+    report() {
+      if (this.x !== 0 && this.y !== 0) {
+        this.rep =  `The robot is at X:${this.x}, Y:${this.y}, facing ${this.direction.toLowerCase()}`;
+      } else {
+        this.rep = 'The robot is not placed yet';
+      }
+      alert(this.rep);
+    },
+    left() {
+      if (!this.placed) {
+        return
+      }
+      if (this.direction === Direction.EAST) {
+        this.direction = Direction.NORTH;
+      } else if (this.direction === Direction.NORTH) {
+        this.direction = Direction.WEST;
+      } else if (this.direction === Direction.WEST) {
+        this.direction = Direction.SOUTH;
+      } else if (this.direction === Direction.SOUTH) {
+        this.direction = Direction.EAST;
+      }
+    },
+    right() {
+      if (!this.placed) {
+        return
+      }
+      this.left(); this.left(); this.left();
+    },
+    move() {
+      if (!this.placed) {
+        return
+      }
+      if (this.direction === Direction.EAST && this.x < this.maxX - 1) {
+          this.x += 1;
+      } else if (this.direction === Direction.SOUTH && this.y > this.minY - 1) {
+          this.y -= 1;
+      } else if (this.direction === Direction.WEST && this.x > this.minX - 1) {
+          this.x -= 1;
+      } else if (this.direction === Direction.NORTH && this.y < this.maxY - 1) {
+          this.y += 1;
+      }
+    },
+  },
+  data() {
+    return {
+      x: 0,
+      y: 0,
+      direction: Direction.NORTH,
+      maxX: 5,
+      maxY: 5,
+      minX: 1,
+      minY: 1,
+      rep: '',
+      placed: false,
+    };
   },
 });
 </script>
